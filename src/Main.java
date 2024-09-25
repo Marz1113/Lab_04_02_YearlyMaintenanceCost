db24df9cd144709cce8599d17966aac77094adc4
marzu <marzuqazam06@gmail.com>
2024-09-24 22:28:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       double winterCost = 40;
+       double springCost = 50;
+       double summerCost = 60;
+       double fallCost = 70;
+
+       double yearlyCost = winterCost + springCost + summerCost + fallCost;
+
+        System.out.println("winter maintenance cost: $" + winterCost);
+        System.out.println("spring maintenance cost: $" + springCost);
+        System.out.println("summer maintenance cost: $" + summerCost);
+        System.out.println("fall maintenance cost: $" + fallCost);
+        System.out.println("total yearly maintenance cost: $" + yearlyCost);
+
+
+    }
+}
